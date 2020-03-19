cbd54afb086193ebf45477fc2d67c63f365f11f0
GodoFredo <godofredo207@gmail.com>
2020-03-19 15:18:44 -0500

    add home kusi doc

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -1,13 +1,20 @@
 const mix = require('laravel-mix')
 
+require('mix-tailwindcss')
+require('laravel-mix-purgecss')
+
+// const rootPath = mix.paths.root.bind(mix.paths)
+
 mix.webpackConfig({
   devtool: 'source-map'
 })
 
-mix.js('js/app.js', 'scripts/')
+mix
+  .js('js/app.js', 'scripts/')
   .setResourceRoot('/assets')
   .setPublicPath('../assets')
   .sass('scss/app.scss', 'styles/')
+  .tailwind('./tailwind.config.js')
   .options({
     processCssUrls: false
   })
@@ -21,9 +28,17 @@ mix.js('js/app.js', 'scripts/')
     ]
   })
 
+// mix.sourceMaps()
+
 // Source Maps only for dev
-mix.sourceMaps()
-// if (mix.inProduction()) {
-// }
-
-
+if (mix.inProduction()) {
+  mix.purgeCss({
+    content: [
+      '../**/*.hbs'
+    ],
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    whitelistPatterns: [/-active$/, /-enter$/, /-leave-to$/]
+  })
+} else {
+  mix.sourceMaps()
+}
